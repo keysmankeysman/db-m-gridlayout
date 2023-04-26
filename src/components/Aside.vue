b43cde656c9d0e818e82867783f5ef856b241be9
Dima <d.klychnikov@petrosoft.su>
2023-04-26 17:11:36 +0300

    some

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -47,25 +47,54 @@
           icon: 'analytics-outline',
           accessProp: 'realTimePage'
         },
-        {
-          title: 'Организация',
-          href: '/organization',
-          icon: 'business-outline',
-          accessProp: 'organizationPage'
-        },
+        // {
+        //   title: 'Организация',
+        //   href: '/organization',
+        //   icon: 'business-outline',
+        //   accessProp: 'organizationPage'
+        // },
         {
           title: 'Отчеты',
           href: '/reports',
           icon: 'document-outline',
           accessProp: 'reports'
         }
-      ]
+      ],
+      links: [
+        {title: "Главный экран", href: "/dashboard", icon: "home-outline", accessProp: "homePage"},
+        {title: "В реальном времени", href: "/realtime", icon: "analytics-outline", accessProp: "realTimePage"},
+        {title: "Организация", href: "/organization", icon: "business-outline", accessProp: "organizationPage"},
+        {title: "IP-адреса", href: "/ipaddress", icon: "book-outline", accessProp: "ipAddresses"},
+        {title: "Домены", href: "/domains", icon: "server-outline", accessProp: "domains"},
+        {title: "Сервисы", href: "/services", icon: "layers-outline", accessProp: "categorization"},
+        {title: "BGP AS", href: "/asn", icon: "grid-outline", accessProp: "bgpAs"},
+        {title: "Клиенты", href: "/clients", icon: "people-outline", accessProp: "clients", visibleByDefault: true},
+        {title: "Настройки", href: "/settings", icon: "cog-outline", accessProp: "settings", visibleByDefault: true},
+        {title: "Отчеты", href: "/reports", icon: "document-outline", accessProp: "reports"},
+        {title: "Учёт потребления продуктов", href: "/reports", icon: "document-outline", accessProp: "reports"}
+
+          // {title: "Профиль", href: "/", icon: "mdi-shield-account"},
+      ],
     }),
     methods: {
       signOut() {
         console.log('анлогин')
         this.$router.push('/login')
       }
+    },
+    created() {
+      console.log(this.$router.getRoutes())
+    },
+    computed: {
+      // accessRoutes() {
+
+        // return this.$store.getters["auth/authTokenItems"].admin ? this.$store.getters["auth/getAccessRoutes"] : this.$router.getRoutes().filter(route => this.$store.getters["auth/authTokenItems"].page.includes(route.path))
+      // },
+      // getAccessLinks() {
+      //   return this.links.filter(link => {
+      //     return this.accessRoutes.find(el => el.path === link.href)
+      //   })
+      // }
     }
   }
 </script>
